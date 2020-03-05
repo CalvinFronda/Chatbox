f2304f9e701fc4d3666fd0ba3ab623594d00b2f0
Calvin Fronda <cfronda209@gmail.com>
2020-03-05 14:27:56 -0800

    Proof of concept

---
+++ Client/src/components/Join/Join.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Join.css'
+
+const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    return (
+        <div className='joinOuterContainer'>
+            <div className='joinInnerContainer'>
+                <h1 className='heading'> Please enter a name & the Room</h1>
+                <div><input placeholder="Name" className='joinInput' type="text" onChange={(event) => setName(event.target.value)} ></input></div>
+                <div><input placeholder="Room" className='joinInput' type="text" onChange={(event) => setRoom(event.target.value)} ></input></div>
+
+                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                    <button className="button" type="submit">Sign in</button>
+                </Link>
+            </div>
+
+        </div>
+    )
+}
+
+export default Join
---
+++ Client/src/components/Message/Message.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Message.css'
+
+
+
+const Message = ({ message: { user, text }, name }) => {
+    let isSentByCurrentUser = false;
+
+    const trimName = name.trim().toLowerCase();
+
+    if (user === trimName) {
+        isSentByCurrentUser = true;
+
+    }
+    return (
+        isSentByCurrentUser ? (
+            <div className="messageContainer justifyEnd">
+                <p className="sentText pr-10">{trimName} </p>
+                <div className="messageBox backgroundBlue">
+                    <p className="message"> {text}</p>
+                </div>
+            </div>
+        )
+            : (
+                <div className="messageContainer justifyStart">
+                    <div className="messageBox backgroundLight" >
+                        <p className="message colorDark"> {text}</p>
+                    </div>
+                    <p className="sentText pl-10">{user} </p>
+                </div>
+            )
+    )
+}
+
+export default Message;
